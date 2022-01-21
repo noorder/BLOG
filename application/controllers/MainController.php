f70f9895b42168b1f6742edb614f6008db238b91
Stas Chekalaev <schekalaev@gmail.com>
2022-01-22 00:25:09 +0300

    1

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -20,6 +20,10 @@ class MainController extends Controller
     public function contactAction()
     {
         if(!empty($_POST)) {
+            if(!$this->model->contactValidate($_POST)) {
+                $this->view->message('error', $this->model->error);
+            }
+            mail('schekalaev@gmail.com', 'Сообщение из блога', $_POST['name'] . '|' . $_POST['email'] . '|' . $_POST['text']);
             $this->view->message('success', 'форма работает');
         }
         $this->view->render('Контакты');
